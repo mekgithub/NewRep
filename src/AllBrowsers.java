abbb63e773c7b490e186308a4739e8bedab912b4
18327 <mkortak@gmail.com>
2019-09-17 19:24:19 -0500

    my first Git commit

---
+++ src/AllBrowsers.java
@@ -0,0 +1,25 @@
+
+    import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+    public class AllBrowsers
+    {
+        public WebDriver  chrome()
+        {
+            System.setProperty("webdriver.chrome.driver","C:\\Users\\iulusoy\\Downloads\\chromedriver_win32\\chromedriver.exe");
+            WebDriver driver=new ChromeDriver();
+            return driver;
+        }
+
+        //this method invokes the firefox driver
+        public void firefox()
+        {
+            System.setProperty("webdriver.gecko.driver","C:\\Users\\iulusoy\\Downloads\\geckodriver-v0.25.0-win64\\geckodriver.exe");
+            WebDriver driver=new FirefoxDriver();
+            driver.get("http://www.flypgs.com");
+        }
+
+    }
+
+
