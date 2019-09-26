efe92d187fa5c5f7708c60f750d661b546bcfc1a
18327 <mkortak@gmail.com>
2019-09-26 11:32:22 -0500

    Contains Fuction-Xpat1

---
+++ src/RelativeAbsoluteXpath.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class RelativeAbsoluteXpath {
+ /*   public static void main(String[] args) throws InterruptedException {
+        Browser myBrowser=new Browser();
+        WebDriver driver=myBrowser.invokeChromeBrowser();
+        driver.get("http://www.expedia.com");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//form[@id='id=\"gcw-packages-form-hp-package']/section/div/div/div/div/label/input")).sendKeys("Atlanta");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='hero-banner']/div/div/section/div/div/div/div"))
+    }*/
+}
